7684120d7f4da6522d9616d7238d557e205594fc
Barış BALLI <barisbll.dev@gmail.com>
2026-04-02 08:45:55 +0200

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const map = new Map();
+        for (const num of nums) {
+            map.set(num, (map.get(num) || 0) + 1);
+        }
+
+        return [...map.entries()].sort((a, b) => b[1] - a[1]).map(([ele,]) => ele).slice(0, k);
+    }
+}
